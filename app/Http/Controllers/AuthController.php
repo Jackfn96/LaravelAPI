9e2f95625828e64f89c7f794e3a76d68e6d258fb
Jack Fraser-Nash <jackfn96@gmail.com>
2020-10-23 13:17:05 +0100

    Registration & listing all users complete

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+  public function register(Request $request)
+  {
+    $user = User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => bcrypt($request->password),
+    ]);
+
+    $token = auth()->login($user);
+
+    return $this->respondWithToken($token);
+  }
+
+  protected function respondWithToken($token)
+  {
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'bearer',
+      'expires_in' => auth()->factory()->getTTL() * 60
+    ]);
+  }
+
+  function list()
+  {
+    return User::all();
+
+  }
+}
